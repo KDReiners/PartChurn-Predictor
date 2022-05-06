61063fb6fa3b5889399e55fa4178a2902f97289c
Klaus-Dieter Riners <kdreiners@gmx.de>
2022-05-06 16:39:52 +0200

    Starting point

--- Shared/PartChurn_PredictorApp.swift
+++ Shared/PartChurn_PredictorApp.swift
@@ -13,8 +13,7 @@ struct PartChurn_PredictorApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            directoryView()
         }
     }
 }
