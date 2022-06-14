5bc62f7d2c0816ee8dc4f18288446024931803fb
Klaus Reiners <klaus.reiners@wolterskluwer.com>
2022-06-14 14:29:11 +0200

    Before batchImport

--- Shared/Structs/CSV_Importer.swift
+++ Shared/Structs/CSV_Importer.swift
@@ -11,6 +11,7 @@ import SwiftUI
 
 struct CSV_Importer {
     internal static func read(url: URL, modelName: String) {
+        var batchArray = Array<coreDataProperties>()
         let columnsViewModel = ColumnsModel()
         let modelsViewModel = ModelsModel()
         let valuesViewModel = ValuesModel()
@@ -19,10 +20,10 @@ struct CSV_Importer {
         let file = filesViewModel.items.first(where: { $0.files2model == model && $0.name == url.lastPathComponent})
         let stream = InputStream(fileAtPath: url.path)
         let reader = try! CSVReader(stream: stream!, hasHeaderRow: true, delimiter: ";")
-        let headerRow = reader.headerRow!
+        let columns = reader.headerRow!
         if !columnsViewModel.items.contains(where: { $0.column2model == model}) {
             var i: Int16 = 0
-            headerRow.forEach { column in
+            columns.forEach { column in
                 let newColumn = columnsViewModel.insertRecord()
                 newColumn.name = column
                 newColumn.orderno = i
@@ -36,19 +37,28 @@ struct CSV_Importer {
             newFile.files2model = model
             var rowCount: Int64 = 0
             while let row = reader.next() {
-                for i in 0..<headerRow.count {
-                    let newValue = valuesViewModel.insertRecord()
-                    let col = columnsViewModel.items.first(where: { $0.name == headerRow[i] && $0.column2model == model})
-                    newValue.value = row[i]
-                    newValue.value2column = col
-                    newValue.rowno = rowCount
-                    newValue.value2model = model
+                for i in 0..<columns.count {
+                    let newEntry = coreDataProperties(columnName: columns[i], rowNo: rowCount, value: row[i])
+                    batchArray.append(newEntry)
+//                    let newValue = valuesViewModel.insertRecord()
+//                    let col = columnsViewModel.items.first(where: { $0.name == headerRow[i] && $0.column2model == model})
+//                    newValue.value = row[i]
+//                    newValue.value2column = col
+//                    newValue.rowno = rowCount
+//                    newValue.value2model = model
                 }
                 rowCount += 1
+                print(rowCount)
             }
         }
-        columnsViewModel.saveChanges()
-        valuesViewModel.saveChanges()
-        modelsViewModel.saveChanges()
+//        columnsViewModel.saveChanges()
+//        valuesViewModel.saveChanges()
+//        modelsViewModel.saveChanges()
+    }
+    struct coreDataProperties {
+        var columnName: String
+        var rowNo: Int64
+        var value: String
+        
     }
 }
