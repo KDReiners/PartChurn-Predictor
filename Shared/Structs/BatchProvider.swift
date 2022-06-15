b4f79989e5d5df70689944130bcbbb3d7597e9d8
Klaus Reiners <klaus.reiners@wolterskluwer.com>
2022-06-15 10:22:47 +0200

    Inter

--- Shared/Structs/BatchProvider.swift
+++ Shared/Structs/BatchProvider.swift
@@ -94,7 +94,44 @@ internal class BatchProvider {
     private var lastToken: NSPersistentHistoryToken?
 
     /// A persistent container to set up the Core Data stack.
-    
+    lazy var container: NSPersistentContainer = {
+        /// - Tag: persistentContainer
+        let container = NSPersistentContainer(name: "Earthquakes")
+
+        guard let description = container.persistentStoreDescriptions.first else {
+            fatalError("Failed to retrieve a persistent store description.")
+        }
+
+        if inMemory {
+            description.url = URL(fileURLWithPath: "/dev/null")
+        }
+
+        // Enable persistent store remote change notifications
+        /// - Tag: persistentStoreRemoteChange
+        description.setOption(true as NSNumber,
+                              forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
+
+        // Enable persistent history tracking
+        /// - Tag: persistentHistoryTracking
+        description.setOption(true as NSNumber,
+                              forKey: NSPersistentHistoryTrackingKey)
+
+        container.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+
+        // This sample refreshes UI by consuming store changes via persistent history tracking.
+        /// - Tag: viewContextMergeParentChanges
+        container.viewContext.automaticallyMergesChangesFromParent = false
+        container.viewContext.name = "viewContext"
+        /// - Tag: viewContextMergePolicy
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        container.viewContext.undoManager = nil
+        container.viewContext.shouldDeleteInaccessibleFaults = true
+        return container
+    }()
 
     /// Creates and configures a private queue context.
     private func newTaskContext() -> NSManagedObjectContext {
