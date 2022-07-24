7f699fee3461eca269f210ec4c995cc6a53df56f
Klaus-Dieter Riners <kdreiners@gmx.de>
2022-07-24 11:20:23 +0200

    Interim

--- Shared/Classes/Composer.swift
+++ Shared/Classes/Composer.swift
@@ -16,7 +16,7 @@ internal class Composer {
     var files: NSSet!
     var mlDataTable_Base: MLDataTable!
     var columnsDataModel = ColumnsModel()
-    var orderedColumns = [Columns]()
+    var orderedColumns: [Columns]
     var timeBasedColumns: [String]
     var primaryKeyColumns: [String]
     
@@ -25,6 +25,7 @@ internal class Composer {
     var cognitionObjects = [CognitionObject]()
     init(model: Models)
     {
+        self.orderedColumns = Array(model.model2columns?.allObjects as! [Columns]).sorted(by: { $0.orderno < $1.orderno})
         self.model = model
         self.files = model.model2files
         self.timeBasedColumns = Array(model.model2columns?.allObjects as! [Columns]).filter({ $0.ispartoftimeseries == 1 }).map( {
@@ -55,82 +56,52 @@ internal class Composer {
         var test: MLDataTable?
         var allInDataTable = MLDataTable()
         for cognitionSource in cognitionSources {
-            let transformedTable: MLDataTable = adjustColumnNames(cognitionSource: cognitionSource)
-            let joinColums = Set(allInDataTable.columnNames).intersection(transformedTable.columnNames)
+            guard let currentMLDataTable = cognitionSource.coreDataML?.mlDataTable else {
+                fatalError("no mldatatable found in cognition source.")
+            }
             if allInDataTable.rows.count == 0 {
-                allInDataTable = transformedTable
+                allInDataTable = currentMLDataTable
             } else {
-                switch joinColums.count {
+                let joinColumns = Set(allInDataTable.columnNames).intersection(currentMLDataTable.columnNames)
+//                joinColumns.remove(at: joinColumns.firstIndex(of: "COGNITIONSOURCE")!)
+                switch joinColumns.count {
                 case 1:
-                    joinParam1 = Array(joinColums)[0]
-                    allInDataTable = allInDataTable.join(with: transformedTable, on: joinParam1)
+                    joinParam1 = Array(joinColumns)[0]
+                    allInDataTable = allInDataTable.join(with: currentMLDataTable, on: joinParam1)
                 case 2:
-                    joinParam1 = Array(joinColums)[0]
-                    joinParam2 = Array(joinColums)[1]
-                    allInDataTable = allInDataTable.join(with: transformedTable, on: joinParam1, joinParam2)
+                    joinParam1 = Array(joinColumns)[0]
+                    joinParam2 = Array(joinColumns)[1]
+                    allInDataTable = allInDataTable.join(with: currentMLDataTable, on: joinParam1, joinParam2)
                 default: print("no join colums")
                 }
             }
+            allInDataTable.removeColumn(named: "COGNITIONSOURCE")
         }
         for cognitionObject in cognitionObjects {
-            let transformedTable: MLDataTable = adjustColumnNames(cognitionObject: cognitionObject)
-            let joinColums = Set(allInDataTable.columnNames).intersection(transformedTable.columnNames)
+            guard let currentMLDataTable = cognitionObject.coreDataML?.mlDataTable else {
+                fatalError("no mldatatable found in coginition source.")
+            }
+            let joinColums = Set(allInDataTable.columnNames).intersection(currentMLDataTable.columnNames)
             switch joinColums.count {
             case 1:
                 joinParam1 = Array(joinColums)[0]
-                allInDataTable = allInDataTable.join(with: transformedTable, on: joinParam1)
+                allInDataTable = allInDataTable.join(with: currentMLDataTable, on: joinParam1)
             case 2:
                 joinParam1 = Array(joinColums)[0]
                 joinParam2 = Array(joinColums)[1]
-                allInDataTable = allInDataTable.join(with: transformedTable, on: joinParam1, joinParam2)
+                allInDataTable = allInDataTable.join(with: currentMLDataTable, on: joinParam1, joinParam2)
             default: print("no join colums")
             }
         }
+        allInDataTable.removeColumn(named: "COGNITIONOBJECT")
+        
         test = allInDataTable.intersect(201712, of: "I_REPORTMONTH")
         print(test)
         return allInDataTable
     }
-    private func adjustColumnNames(cognitionSource: CognitionSource) -> MLDataTable{
-        let prefix = cognitionSource.name
-        var mlDataTable_Adjusted = cognitionSource.coreDataML?.mlDataTable
-        for column in cognitionSource.coreDataML!.orderedColumns {
-            let columnName = column.name!
-            column.alias = column.name!
-            let pattern = String(column.ispartofprimarykey == true ? 1: 0) + String(column.isincluded == true ? 1: 0) + String(column.isshown == true ? 1: 0)
-            if columnName != "COGNITIONSOURCE" && column.isincluded == true {
-                let alias = prefix! +  "\n" + columnName
-//                mlDataTable_Adjusted?.renameColumn(named: columnName, to: alias)
-                column.alias = alias
-                self.orderedColumns.append(column)
-            } else if columnName != "COGNITIONSOURCE" && column.isincluded == false && column.isshown == true {
-                self.orderedColumns.append(column)
-            } else {
-                mlDataTable_Adjusted?.removeColumn(named: "COGNITIONSOURCE")
-            }
-        }
-        return mlDataTable_Adjusted!
-    }
-    private func adjustColumnNames(cognitionObject: CognitionObject) -> MLDataTable{
-        let prefix = cognitionObject.name
-        var mlDataTable_Adjusted = cognitionObject.coreDataML?.mlDataTable
-        for column in cognitionObject.coreDataML!.orderedColumns {
-            let columnName = column.name!
-            column.alias = column.name!
-            if columnName != "COGNITIONOBJECT" && column.istarget == true && column.ispartofprimarykey == false {
-                let alias = prefix! +  "\n" + columnName
-//                mlDataTable_Adjusted?.renameColumn(named: columnName, to: alias)
-                column.alias = alias
-                self.orderedColumns.append(column)
-            } else if columnName != "COGNITIONOBJECT" && column.istarget == false && column.ispartofprimarykey == true {
-                self.orderedColumns.append(column)
-            } else if columnName != "COGNITIONOBJECT" && column.ispartofprimarykey == true {
-                column.isshown = false
-            }
-            else {
-                mlDataTable_Adjusted?.removeColumn(named: "COGNITIONOBJECT")
-            }
-        }
-        return mlDataTable_Adjusted!
+    private func reduceColumns(allInDataTable: MLDataTable ) {
+        var result: MLDataTable
+        result = allInDataTable[primaryKeyColumns]
     }
     static func getColumnPivotValue(pivotColum: Columns?) ->String? {
         return Composer.valuesDataModel.items.filter { $0.value2column == pivotColum}.first?.value
