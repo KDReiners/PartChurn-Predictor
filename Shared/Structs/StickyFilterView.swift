81d84dd7869eb819ee24d33e468a6da7cb91a2ed
Klaus-Dieter Riners <kdreiners@gmx.de>
2022-08-14 17:03:15 +0200

    inerim

---
+++ Shared/Structs/StickyFilterView.swift
@@ -0,0 +1,60 @@
+//
+//  StickyFilterView.swift
+//  PartChurn Predictor
+//
+//  Created by Klaus-Dieter Reiners on 14.08.22.
+//
+
+import Foundation
+import SwiftUI
+class filterViewProvider: ObservableObject {
+    var mlDataTableFactory: MlDataTableFactory
+    var tableFilterView: TableFilterView
+    init(mlDataTableFactory: MlDataTableFactory) {
+        self.mlDataTableFactory = mlDataTableFactory
+        tableFilterView = TableFilterView(columns: self.mlDataTableFactory.customColumns, gridItems: self.mlDataTableFactory.gridItems, mlDataTableFactory: self.mlDataTableFactory)
+    }
+
+}
+struct TableFilterView: View {
+    var gridItems: [GridItem]
+    var columns: [CustomColumn]
+    @State var filterDict: Dictionary<String, String>!
+    var mlDataTableFactory: MlDataTableFactory
+    init(columns: [CustomColumn], gridItems: [GridItem], mlDataTableFactory: MlDataTableFactory) {
+        self.columns = columns
+        self.gridItems = gridItems
+        self.filterDict = Dictionary<String, String>()
+        self.mlDataTableFactory = mlDataTableFactory
+        for column in columns {
+            filterDict[column.title] = ""
+        }
+    }
+    var body: some View {
+        LazyVGrid(columns: gridItems) {
+            ForEach(columns) { col in
+                TextField(col.title, text: binding(for: col.title)).frame(alignment: .trailing)
+                    .onSubmit {
+                        print(binding(for: col.title))
+                        self.mlDataTableFactory.loaded = false
+                        self.mlDataTableFactory.filterMlDataTable(filterDict: filterDict)
+                    }
+            }
+        }
+    }
+    private func binding(for key: String) -> Binding<String> {
+        return Binding(get: {
+            return self.filterDict[key] ?? ""
+        }, set: {
+            if !$0.isEmpty {
+                self.filterDict[key] = $0
+            } else {
+                if self.filterDict[key] != nil {
+
+                    self.filterDict.removeValue(forKey: key)
+                }
+            }
+        })
+    }
+    
+}
--- Shared/Views/ValuesView.swift
+++ Shared/Views/ValuesView.swift
@@ -106,53 +106,11 @@ struct ValuesView: View {
                 .frame(maxWidth: .infinity)
                 .frame(maxHeight: .infinity)
                 .overlay(
-                    stickyFilterView(columns: mlDataTableFactory.customColumns, gridItems: mlDataTableFactory.gridItems, mlDataTableFactory: self.mlDataTableFactory)
+                    TableFilterView(columns: mlDataTableFactory.customColumns, gridItems: mlDataTableFactory.gridItems, mlDataTableFactory: self.mlDataTableFactory)
                 )
         }
         .background(.white)
         .padding(.bottom)
-    }
-    struct stickyFilterView: View {
-        var gridItems: [GridItem]
-        var columns: [CustomColumn]
-        @State var filterDict: Dictionary<String, String>!
-        var mlDataTableFactory: MlDataTableFactory
-        init(columns: [CustomColumn], gridItems: [GridItem], mlDataTableFactory: MlDataTableFactory) {
-            self.columns = columns
-            self.gridItems = gridItems
-            self.filterDict = Dictionary<String, String>()
-            self.mlDataTableFactory = mlDataTableFactory
-            for column in columns {
-                filterDict[column.title] = ""
-            }
-        }
-        var body: some View {
-            LazyVGrid(columns: gridItems) {
-                ForEach(columns) { col in
-                    TextField(col.title, text: binding(for: col.title)).frame(alignment: .trailing)
-                        .onSubmit {
-                            print(binding(for: col.title))
-                            self.mlDataTableFactory.loaded = false
-                            self.mlDataTableFactory.filterMlDataTable(filterDict: filterDict)
-                        }
-                }
-            }
-        }
-        private func binding(for key: String) -> Binding<String> {
-            return Binding(get: {
-                return self.filterDict[key] ?? ""
-            }, set: {
-                if !$0.isEmpty {
-                    self.filterDict[key] = $0
-                } else {
-                    if self.filterDict[key] != nil {
-
-                        self.filterDict.removeValue(forKey: key)
-                    }
-                }
-            })
-        }
-        
     }
 }
 
